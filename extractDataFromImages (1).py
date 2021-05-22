464380e421a3f45f6ea104a433251ee35543d3a0
JoelZantiel <78826255+JoelZantiel@users.noreply.github.com>
2021-05-22 17:54:47 -0500

    Initial commit

---
+++ extractDataFromImages (1).py
@@ -0,0 +1,81 @@
+# -*- encoding: utf-8 -*-
+
+from PIL.ExifTags import GPSTAGS
+from PIL.ExifTags import TAGS
+from PIL import Image
+import os
+import argparse
+
+def ValidatePath(thePath):
+       
+    # Validate the path exists
+    if not os.path.exists(thePath):
+        raise argparse.ArgumentTypeError('Path does not exist')
+
+    # Validate the path is readable
+    if os.access(thePath, os.R_OK):
+        return thePath
+    else:
+        raise argparse.ArgumentTypeError('Path is not readable')
+
+def decode_gps_info(exif):
+    gpsinfo = {}
+    if 'GPSInfo' in exif:
+        #Parse geo references.
+        Nsec = exif['GPSInfo'][2][2] 
+        Nmin = exif['GPSInfo'][2][1]
+        Ndeg = exif['GPSInfo'][2][0]
+        Wsec = exif['GPSInfo'][4][2]
+        Wmin = exif['GPSInfo'][4][1]
+        Wdeg = exif['GPSInfo'][4][0]
+        if exif['GPSInfo'][1] == 'N':
+            Nmult = 1
+        else:
+            Nmult = -1
+        if exif['GPSInfo'][1] == 'E':
+            Wmult = 1
+        else:
+            Wmult = -1
+        Lat = Nmult * (Ndeg + (Nmin + Nsec/60.0)/60.0)
+        Lng = Wmult * (Wdeg + (Wmin + Wsec/60.0)/60.0)
+        exif['GPSInfo'] = {"Lat" : Lat, "Lng" : Lng}
+        #input()
+
+ 
+def get_exif_metadata(image_path):
+    ret = {}
+    image = Image.open(image_path)
+    if hasattr(image, '_getexif'):
+        exifinfo = image._getexif()
+        if exifinfo is not None:
+            for tag, value in exifinfo.items():
+                decoded = TAGS.get(tag, tag)
+                ret[decoded] = value
+    decode_gps_info(ret)
+    return ret
+    
+def printMeta(ruta):
+    os.chdir(ruta)
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            print(os.path.join(root, name))
+            print ("[+] Metadata for file: %s " %(name))
+            #input()
+            try:
+                exifData = {}
+                exif = get_exif_metadata(name)
+                for metadata in exif:
+                    print ("Metadata: %s - Value: %s " %(metadata, exif[metadata]))
+                print ("\n")
+            except:
+                import sys, traceback
+                traceback.print_exc(file=sys.stdout)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--Path',
+                    required=True, help="Specify the target folder to baseline")
+    params = parser.parse_args()
+    targetPath = params.Path
+    printMeta(targetPath)
+
+
